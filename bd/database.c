704deb974204c6ee57a9837ee68bb03bb7b66401
Matheus Cerqueira <matheus.crqr@outlook.com>
2020-07-14 22:00:28 -0300

    Adding comments on main

--- bd/database.c
+++ bd/database.c
@@ -11,7 +11,8 @@
 void cleanInput() {
     int c;
     //Consome todos os caracteres de stdin
-    while (((c = fgetc(stdin)) != '\n') && (c != EOF));
+    while (((c = fgetc(stdin)) != '\n') && (c != EOF))
+        ;
 }
 
 int main() {
@@ -20,33 +21,46 @@ int main() {
     int index;
     boolean indexCreated = FALSE;
 
+    //Recebe o nome do arquivo de metadados
+    //e popula a struct
     metadataFileName = getLineFromFile(stdin);
-
     Metadata *metadata = NULL;
     metadata = readMetaData(metadataFileName);
 
+    //Inicializa o registro auxiliar
     Register *reg = NULL;
-    int registerSize = 0;
 
+    //Loop até exit
     do {
+        //Recebe a opção da linha
         opt = getOptionFromFile(stdin);
 
+        //Trata a opção
         switch (opt) {
             case INSERT:
+                //Lê, escreve e libera o registro
                 readRegisterFromString(&reg, getLineFromFile(stdin), metadata);
                 writeRegisterToFile(reg, metadata);
                 freeRegister(&reg, metadata);
                 break;
 
             case INDEX:
+                //Escreve o arquivo de index e seta
+                //o operador de criação
                 writeIndex(metadata);
                 indexCreated = TRUE;
                 break;
 
             case SEARCH:
+                //Se o operador de criação do index não
+                //estiver ativado, cria o arquivo
                 if (!indexCreated) writeIndex(metadata);
+
+                //Lê o index e trata stdin
                 scanf(" %d", &index);
                 cleanInput();
+
+                //Se o index for encontrado, exibe e libera
                 if (readRegisterFromFile(metadata, &reg, index)) {
                     printRegister(reg, metadata);
                     freeRegister(&reg, metadata);
@@ -58,6 +72,7 @@ int main() {
         }
     } while (opt != EXIT);
 
+    //Libera os ponteiros utilizados
     freeMetadata(metadata);
     free(metadataFileName);
 
